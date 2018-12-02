64094435ede9c001d782fe9ebea94e92adf686d8
unknown <pricebailey123@gmail.com>
2018-12-01 21:15:51 -0600

    added dice roll capability

--- dice.js
+++ dice.js
@@ -1,9 +1,11 @@
-$(function(){
-    $("#rollDice").click(function(){
-alert("The button was clicked");
+$(function() {
+  $("#rollDice").click(function() {
+    let tempRoll = (Math.floor(Math.random() * 6) + 1);
+
+    $("#diceResult").html("<p>Roll: " + tempRoll + "</p><br>")
 
 
-    })
+  });
 
-    //gayyyyyyyyyyyy
-})
+
+});
